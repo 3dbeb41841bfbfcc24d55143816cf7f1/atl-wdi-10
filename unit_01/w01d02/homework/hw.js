85a37b0e640b795cd67f3c9c2b79e2e23877e282
Audrey <s00m3b0di3@gmail.com>
2016-12-07 10:04:31 -0500

    Homework

--- unit_01/w01d02/homework/hw.js
+++ unit_01/w01d02/homework/hw.js
@@ -8,19 +8,24 @@
 // For each expression below, indicate whether it is 'truthy' or 'falsey'
 // by assigning the appropriate variable a value of either `true` or `false`.
 
-// 1. true || false
+// 1. true || false 
+//var resOne = true;
 var resOne;
 
 // 2. null || ''
+//var resTwo = falsey;
 var resTwo;
 
 // 3. 'false' && true
+//var resThree = true;
 var resThree;
 
 // 4. !!0
+//var resFour = false;
 var resFour;
 
 // 5. !null && !undefined
+//var resFive = true;
 var resFive;
 
 /// Flow Control ///
@@ -28,13 +33,30 @@ var resFive;
 // 6. Write a `while` loop that increases `resSix` by 23 until `resSix` is five
 //    digits long. (Hint: take a look at the .toString() method)
 var resSix = 5;
-// Replace This Comment With Your Code
+console.log(resSix.toString().length);
+
+while (resSix.toString().length < 5){
+  resSix+=23;
+}
+console.log(resSix)
 
 // 7. Write a `while` loop that increases `resSeven` by 12 when it's strictly
 //    below 95, decreases it by 7 when it's strictly above 105, and stops as
 //    soon as `resSeven` equals or falls between those two values.
 var resSeven = 65;
-// Replace This Comment With Your Code
+
+do {
+  resSeven === 95 || 105; 95<resSeven< 105;
+}
+while (resSeven < 95){
+    resSeven =-7
+}
+while (resSeven > 105) {
+resSeven+=12
+}
+{
+  console.log(resSeven)
+}
 
 // 8. Write a `for` loop that counts from 0 (inclusive) to 1857 (exclusive) and
 //    calculates the sum of all of those numbers, storing the result in the
