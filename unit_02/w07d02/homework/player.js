4af227e470c5e3e463873ebca8ed47e2d1bbf6c1
Christine Yi <christineyi@Christines-MacBook-Pro.local>
2017-01-17 17:04:43 -0500

    pushing w07d02 hw

---
+++ unit_02/w07d02/homework/player.js
@@ -0,0 +1,17 @@
+console.log('player file is loaded');
+
+var player = {
+  name: "Neo",
+  feedTamagotchi: function(tamagotchi) {
+    tamagotchi.foodInTummy++;
+    console.log('YUM!!! ' + tamagotchi.name + ' has been fed and now has: ' + tamagotchi.foodInTummy + ' in the tummy');
+  },
+  medicateTamagotchi: function(tamagotchi) {
+    tamagotchi.health++;
+    console.log(tamagotchi.name + ' has been medicated and now has health: ' + tamagotchi.health);
+  },
+  knockOutTamagotchi: function(tamagotchi) {
+    tamagotchi.restedness++;
+    console.log(tamagotchi.name + ' got some Zzzzs! Restedness: ' + tamagotchi.restedness);
+  }
+}; //end player
