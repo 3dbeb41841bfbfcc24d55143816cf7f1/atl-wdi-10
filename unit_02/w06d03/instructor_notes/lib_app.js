16cde5d7ec7dd2f406b76331b114b8b98272639a
Colin Hart <me@colin-hart.com>
2017-01-18 14:35:31 -0500

    mongo installation

---
+++ unit_02/w06d03/instructor_notes/lib_app.js
@@ -0,0 +1,46 @@
+THIS ONLY WORKS ASSUMING WE ONLY HAVE ONE COPY OF EACH BOOK
+
+Books
+  - isbn: integer
+  - author: string
+  - title: string
+  - checked_out: boolean
+  - maintenance
+  - current_owner: user_id, library_card_number
+  - [... ALL OF THE FREAKIN BOOK DATA]
+  - quality: string number
+  - holds / waitlist
+
+Q: how do we keep of checked out books?
+
+library cards
+User
+  - current_books: array of book objects
+  - fees: number
+  - name
+  - library_card_id_number: number
+  address
+    - city, 
+    - state
+    - zip
+  - account_balance: number for copying 
+  - email
+
+DUE DATES, are they standardized? due dates either based on the user, or maybe they're based on desire for book
+
+rooms
+
+authors
+genres
+subjects
+Computers
+videos
+audio books
+shelves
+desks
+copiers
+Employees
+payroll
+timesheets
+branches
+  
