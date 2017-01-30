8885aaf6f51e9d8425bfd39347b29c9603e35dee
Marc Wright <marcwright@Marcs-MacBook-Pro-2.local>
2017-01-30 17:51:38 -0500

    update http solutions

--- unit_03/w09d04/student_labs/angular-http-lesson/presidents-app-starter-code-single-app-version/models/President.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var PresidentSchema = mongoose.Schema({
-	name: String,
-  start: Number,
-	end: Number
-});
-
-module.exports = mongoose.model('President', PresidentSchema);
