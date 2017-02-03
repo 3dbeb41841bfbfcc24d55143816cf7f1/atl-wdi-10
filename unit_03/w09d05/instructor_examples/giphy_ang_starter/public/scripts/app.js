b9b5754eb8272a433d5e7f68ee7af8f613068939
Colin Hart <me@colin-hart.com>
2017-02-03 10:35:28 -0500

    readding starter code

---
+++ unit_03/w09d05/instructor_examples/giphy_ang_starter/public/scripts/app.js
@@ -0,0 +1,10 @@
+  angular.module('giphyAngularApp')
+    .controller('HomeController', HomeController)
+    .controller('GiphyController', GiphyController)
+
+    function HomeController() {
+    }
+
+    function GiphyController($http) {
+
+    }
