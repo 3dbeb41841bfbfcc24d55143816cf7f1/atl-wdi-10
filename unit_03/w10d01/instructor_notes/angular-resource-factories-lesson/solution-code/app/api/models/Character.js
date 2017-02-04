f3e2b0c48d2f9396b8533118d322cc65e8445077
Marc Wright <marcwright@Marcs-MacBook-Pro-2.local>
2017-02-04 12:57:29 -0500

    update angular services lesson

--- unit_03/w10d01/instructor_notes/angular-resource-factories-lesson/solution-code/app/api/models/Character.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var CharacterSchema = mongoose.Schema({
-	name: String,
-  lightsaber: String
-});
-
-module.exports = mongoose.model('Character', CharacterSchema);
-
--- unit_03/w10d01/instructor_notes/angular-resource-factories-lesson/solution-code/app/front-end/js/app.js
+++
@@ -1,2 +0,0 @@
-angular
-  .module("lightsaberApp", ['ngResource']);
