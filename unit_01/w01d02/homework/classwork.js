2ccd10e69375f2af60c3e51d0403eae9deea56b4
MarenWoodruff <wren403@gmail.com>
2017-02-27 09:31:55 -0500

    updated morning exercise

--- unit_01/w01d02/homework/classwork.js
+++ unit_01/w01d02/homework/classwork.js
@@ -10,48 +10,36 @@
 
 // 1. true || false 
 var resOne;
+// Replace This Comment With Your Code
 
 // 2. null || ''
 var resTwo;
+// Replace This Comment With Your Code
 
 // 3. 'false' && true
 var resThree;
+// Replace This Comment With Your Code
 
 // 4. !!0
 var resFour;
+// Replace This Comment With Your Code
 
 // 5. !null && !undefined
 var resFive;
+// Replace This Comment With Your Code
 
 /// Flow Control ///
 
 // 6. Write a `while` loop that increases `resSix` by 23 until `resSix` is five
 //    digits long. (Hint: take a look at the .toString() method)
-var resSix = 5;
-console.log(resSix.toString().length);
-
-while (resSix.toString().length < 5){
-  resSix+=23;
-}
-console.log(resSix)
+var resSix;
+// Replace This Comment With Your Code
 
 // 7. Write a `while` loop that increases `resSeven` by 12 when it's strictly
 //    below 95, decreases it by 7 when it's strictly above 105, and stops as
 //    soon as `resSeven` equals or falls between those two values.
-var resSeven = 65;
-
-do {
-  resSeven === 95 || 105; 95<resSeven< 105;
-}
-while (resSeven < 95){
-    resSeven =-7
-}
-while (resSeven > 105) {
-resSeven+=12
-}
-{
-  console.log(resSeven)
-}
+var resSeven;
+// Replace This Comment With Your Code
 
 // 8. Write a `for` loop that counts from 0 (inclusive) to 1857 (exclusive) and
 //    calculates the sum of all of those numbers, storing the result in the
