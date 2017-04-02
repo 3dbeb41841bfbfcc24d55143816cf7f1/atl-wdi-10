e852e2b414875e5057c81a316a7a40a9f6a0b6a3
Gerry Pass <rgpass@gmail.com>
2017-04-02 10:18:54 -0400

    Fix missing deps

--- unit_03/w09d04/student_labs/presidents-app-starter-code-single-app-version/public/js/presidentsController.js
+++ unit_03/w09d04/student_labs/presidents-app-starter-code-single-app-version/public/js/presidentsController.js
@@ -6,7 +6,8 @@ function PresidentsController(){
     {name: 'George Washington', start: 1789, end: 1797 },
     {name: 'John Adams', start: 1797, end: 1801 },
     {name: 'Thomas Jefferson', start: 1801, end: 1809 },
-    {name: 'James Madison', start: 1809, end: 1817 }
+    {name: 'James Madison', start: 1809, end: 1817 },
+    {name: 'Joshua Quincy Kushner', start: 2020, end: 2028 },
   ];
   this.addPresident = addPresident;
   this.newPresident = {};
