1a68fa9686eed55def07418bd70eb8a4558a6b52
Danny Hurley <hurley.dannyp@gmail.com>
2017-04-04 22:56:00 -0400

    Fixing set-up for webpack lesson and updating set-up for angular-components lesson

--- angular_lessons/labs/criminals-lab-with-services-and-ng-forms/public/js/criminalsController.js
+++ angular_lessons/labs/criminals-lab-with-services-and-ng-forms/public/js/criminalsController.js
@@ -1,9 +1,9 @@
 angular.module('InfamousCriminals')
 .controller('CriminalsController', CriminalsController);
 
-CriminalsController.$inject = ['$http'];
+CriminalsController.$inject = ['CriminalsService'];
 
-function CriminalsController($http){
+function CriminalsController(CriminalsService){
   var self = this;
   self.all = [];
   self.addCriminal = addCriminal;
@@ -13,25 +13,20 @@ function CriminalsController($http){
 
   getCriminals();
   function getCriminals(){
-    $http
-      .get('/criminals')
-      .then(function(response){
-        self.all = response.data.criminals;
+    CriminalsService.getCriminals().then(function (criminalsData) {
+      self.all = criminalsData;
     });
   }
 
   function addCriminal(){
-    $http
-      .post('/criminals', self.newCriminal)
-      .then(function(response){
-        getCriminals();
-    });
-    self.newCriminal = {};
+   CriminalsService.addCriminal(self.newCriminal).then(function(){
+     self.getCriminals();
+       self.newCriminal = {};
+   });
   }
 
   function deleteCriminal(criminal){
-    $http
-      .delete("/criminals/" + criminal._id)
+    CriminalsService.deleteCriminal(criminal)
       .then(function(response){
         var index = self.all.indexOf(criminal);
         self.all.splice(index, 1);
