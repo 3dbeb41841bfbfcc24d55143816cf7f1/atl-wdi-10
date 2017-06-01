b7b5b16c29560d45a7652b3d161e1d9b1a968c52
MarenWoodruff <wren403@gmail.com>
2017-06-01 16:49:26 -0400

    updated angular http lesson

--- labs/unit_03/angular/presidents-app/presidents-app-starter-code/public/js/presidentsController.js
+++ labs/unit_03/angular/presidents-app/presidents-app-starter-code/public/js/presidentsController.js
@@ -6,8 +6,7 @@ function PresidentsController(){
     {name: 'George Washington', start: 1789, end: 1797 },
     {name: 'John Adams', start: 1797, end: 1801 },
     {name: 'Thomas Jefferson', start: 1801, end: 1809 },
-    {name: 'James Madison', start: 1809, end: 1817 },
-    {name: 'Joshua Quincy Kushner', start: 2021, end: 2029 },
+    {name: 'James Madison', start: 1809, end: 1817 }
   ];
   this.addPresident = addPresident;
   this.newPresident = {};
